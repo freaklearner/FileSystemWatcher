33638eba2a50c594ab8d696cb9c1e985bd5af7cc
wowfluxuser <shub.shubhamthakur@gmail.com>
2019-11-14 14:36:08 +0530

    Add project files.

---
+++ FileWatcherSystem/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcherSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run(@"C:\Users\instaclickuser\Desktop\Json_Location\watcherLoc");
+            while (Console.Read() != 'q')
+            {
+
+            }
+
+        }
+
+        private static void Run(string path)
+        {
+            using(FileSystemWatcher watcher = new FileSystemWatcher())
+            {
+                watcher.Path = path;
+
+                watcher.NotifyFilter = NotifyFilters.CreationTime;
+
+                watcher.Filter = "*.*";
+
+                watcher.Changed += (s, e) => change(s, e, path);
+
+                watcher.EnableRaisingEvents = true;
+
+            }
+        }
+
+        private static void change(object source, FileSystemEventArgs e, string path)
+        {
+
+        }
+    }
+}
